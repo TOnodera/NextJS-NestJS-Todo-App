b70a5cfd17dc6869adda211037e7ccc113bd07d9
t.onodera <t.onodera.1.2.1.5@gmail.com>
2024-05-29 23:51:09 +0900

    テスト用DBでの自動テスト実施まで。

--- nestjs/src/todo/todo.resolver.spec.ts
+++ nestjs/src/todo/todo.resolver.spec.ts
@@ -1,19 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TodoResolver } from './todo.resolver';
 import { TodoService } from './todo.service';
+import { CreateTodoInput } from './dto/create-todo.input';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 describe('TodoResolver', () => {
   let resolver: TodoResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TodoResolver, TodoService],
+      providers: [TodoResolver, TodoService, PrismaService],
     }).compile();
 
     resolver = module.get<TodoResolver>(TodoResolver);
   });
 
-  it('should be defined', () => {
-    expect(resolver).toBeDefined();
+  it('createTodoで正常なデータの場合は登録できる', async () => {
+    const data: CreateTodoInput = {
+      title: 'タイトル',
+      description: '詳細',
+    };
+    const result = await resolver.createTodo(data);
+    expect(result.title).toBe(data.title);
+    expect(result.description).toBe(data.description);
   });
 });
