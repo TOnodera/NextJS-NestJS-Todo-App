33882e33b48dd9afd81270e8a8dfac28455210f3
t.onodera <t.onodera.1.2.1.5@gmail.com>
2024-06-03 23:05:47 +0900

    user用モジュール追加

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -7,6 +7,7 @@ import { join } from 'path';
 import { TodoModule } from './todo/todo.module';
 import { PrismaService } from './prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     TodoModule,
     AuthModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
