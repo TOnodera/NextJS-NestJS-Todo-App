a8d2b8be72832800021b1c61aeaf6fb31f095b07
t.onodera <t.onodera.1.2.1.5@gmail.com>
2024-06-10 21:47:07 +0900

    パスワードをハッシュ化して保存

--- nestjs/prisma/seeder.ts
+++ nestjs/prisma/seeder.ts
@@ -1,13 +1,16 @@
 import { PrismaClient } from '@prisma/client';
+import { hash } from 'bcryptjs';
+import { HASH_ROUND } from 'src/const';
 
 const prisma = new PrismaClient();
 
 async function main() {
+  const hashedPassword = await hash('hogefuga', HASH_ROUND);
   await prisma.users.create({
     data: {
       name: 'test-user1',
       email: 'test-user1@example.com',
-      password: 'hogefuga',
+      password: hashedPassword,
     },
   });
 }
--- nestjs/src/auth/auth.service.ts
+++ nestjs/src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { compare } from 'bcryptjs';
 import { UserService } from 'src/user/user.service';
 
 @Injectable()
@@ -9,9 +10,8 @@ export class AuthService {
     private readonly jwtService: JwtService,
   ) {}
   async login(email: string, pass: string): Promise<{ accessToken: string }> {
-    // TODO hash化したパスワードで実行するようにする
     const user = await this.userService.findByEmail(email);
-    if (user?.password !== pass) {
+    if (await compare(user?.password, pass)) {
       throw new UnauthorizedException();
     }
     const payload = { username: user.name, sub: user.id };
